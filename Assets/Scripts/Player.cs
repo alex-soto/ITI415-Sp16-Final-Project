32290347a1c5562d9a4987c487392120fa2b19f3
Alex Soto <as1694@scarletmail.rutgers.edu>
2016-04-18 12:43:48 -0400

    Updated Player.cs
    Added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,48 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public float moveSpeed;
+	private bool canJump = false;
+	private BoxCollider2D playerCollider;
+
+	[Header("Inspector variables")]
+	public float moveSpeed = 2;
+	public float jumpDistance = 25;
 	public int moveDirection;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Awake(){
+		playerCollider = GetComponent<BoxCollider2D>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		Move ();
+
 	}
+
+	// Allows player to jump when touching a surface
+	void OnCollisionEnter2D(Collision2D other){
+		canJump = true;
+	}
+
+	// If player stops touching a surface, they can no longer jump
+	void OnCollisionExit2D(){
+		canJump = false;
+	}
+
+	void Move(){
+		float xAxis = Input.GetAxis("Horizontal");
+		float yAxis = Input.GetAxis("Vertical");
+		Vector3 pos = transform.position;
+
+		pos.x += xAxis * moveSpeed * Time.deltaTime;
+		if (canJump && Input.GetKeyDown("space")) {
+			pos.y += jumpDistance * Time.deltaTime;
+			canJump = false;
+		} 
+		transform.position = pos;
+
+		print ("xAxis: " + xAxis + " yAxis: " + yAxis);
+
+	}
+
+
 }
