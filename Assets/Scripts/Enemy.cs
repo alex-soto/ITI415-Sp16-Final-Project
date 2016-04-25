1b8d8c107672717dd9bc92c3a76540dee1f2fd95
Alex Soto <as1694@scarletmail.rutgers.edu>
2016-04-25 18:43:10 -0400

    Enemy script update
    Starting updating the enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+	/*
+	 * Old code:
+	 * 
 	public bool isFriendly;
 	public bool isAttacking = false;
 	public GameObject attackTarget;
@@ -16,6 +19,7 @@ public class Enemy : MonoBehaviour {
 	private float jumpTime;
 	private float lastTimeMoved = 0;
 	private Rigidbody2D rigidBody;
+
 
 	void Awake(){
 		rigidBody = GetComponent<Rigidbody2D> ();
@@ -83,5 +87,90 @@ public class Enemy : MonoBehaviour {
 	void OnDrawGizmos(){
 		Gizmos.color = Color.red;
 		Gizmos.DrawWireSphere(transform.position, attackDistance);
+	}
+	 */
+
+	/*
+	 * New code: 
+	 * 
+	 * Overview: enemies will follow the player and attempt to hit them with ink projectiles. 
+	 * If the enemy comes into contact with the player’s line, they will become friendly 
+	 * (change color to give player feedback.) Friendly enemies will follow the player by default, 
+	 * but will attack non-friendly enemies if the latter enters the former’s trigger radius.
+	 */
+
+	// If target != null
+
+	public GameObject target;
+	public bool isFriendly;
+	public float attackDelay;
+	public Color startColor = Color.red;
+	public Color friendlyColor = Color.black;
+	
+	//private RaycastHit2D alertRaycast;
+	private RaycastHit2D attackRaycast;
+	private float attackDistance;
+	private RaycastHit2D targetRaycast;
+	private RaycastHit2D navigationRaycast;
+
+	void Awake(){
+		isFriendly = false;
+		attackDistance = 25f;
+	}
+
+	void FixedUpdate(){
+		if (target != null) {
+			// Behavior if a target has been set
+			if (Vector3.Distance(transform.position, target.transform.position) <= attackDistance){
+				Attack (target);
+			} else {
+				Move (target);
+			}
+		} else {
+			// Behavior if there is no target
+			Idle ();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (!isFriendly && col.gameObject.tag == "Player") {
+			target = col.gameObject;
+		} else if (isFriendly && col.gameObject.tag != "Player"){
+			target = col.gameObject;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		CheckForTarget ();
+	}
+
+	void Move(GameObject target){
+		
+	}
+	
+	void Attack(GameObject target){
+		
+	}
+	
+	void Idle(){
+		
+	}
+	
+	void CheckForTarget(){
+		
+	}
+	
+	private Vector3 CalculateSafeJump(){
+		//TODO: Delete the following temporary return value
+		return Vector3.zero;
+		
+	}
+	
+	private void OnProjectileHit(){
+		
+	}
+	
+	private void OnLineHit(){
+		
 	}
 }
